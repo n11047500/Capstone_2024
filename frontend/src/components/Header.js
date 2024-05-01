5942b3dc5723054ea2784afcc1f167f062b42a23
n11047500 <n11047500@qut.edu.au>
2024-05-01 16:55:59 +1000

    Hamburger dropdown funtional

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logoImage from '../assets/logo.png';
 
-
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);  // State to manage menu visibility
+
   return (
     <header className="site-header">
       {/* Menu icon (Hamburger icon) */}
-      <div className="menu-icon">☰</div>
+      <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>☰</div>
+
+      {/* Navigation Links */}
+      {isOpen && (
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      )}
 
       {/* Logo */}
       <img src={logoImage} alt="EZee Planter Boxes" className="logo" />
